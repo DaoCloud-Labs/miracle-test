a47f348a275d4b40360a1bccb3891c205f3e77df
will <wei.shi@daocloud.io>
2019-04-11 14:42:53 +0800

    add api test

---
+++ api_test.robot
@@ -0,0 +1,33 @@
+*** Settings ***
+Documentation       Miracle Service API Testing
+Library             keywords/TestAPI.py
+
+*** Variables ***
+${前段地址}         http://172.16.111.31:31080
+${后端地址}         http://172.16.111.31:31880
+
+*** Test Cases ***
+TestCase-001 Health Check
+    [Tags]    API
+    初始化测试环境     ${后端地址}
+    装载测试数据       预期状态码=200      预期返回值=success
+    测试接口          方法=GET    接口=/health
+
+TestCase-002 Error 404
+    [Tags]    API
+    初始化测试环境     ${后端地址}
+    装载测试数据       预期状态码=404      预期返回值=Not Found
+    测试接口          方法=GET    接口=/404
+
+*** Keywords ***
+初始化测试环境
+    [Arguments]     ${url}
+    INIT TEST ENV   ${url}
+
+装载测试数据
+    [Arguments]     ${预期状态码}        ${预期返回值}
+    LOAD TEST DATA      status_code=${预期状态码}        response_content=${预期返回值}
+
+测试接口
+    [Arguments]     ${方法}     ${接口}
+    REQUEST VALIDATION      method=${方法}     path=${接口}
---
+++ keywords/TestAPI.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+
+
+class TestAPI(object):
+    def __init__(self):
+        self.url = ''
+        self.service_url = ''
+        self.request_data = ''
+        self.expected_status_code = ''
+        self.expected_response_content = ''
+        self.test_data = {}
+
+    def init_test_env(self, url: str):
+        self.url = url
+
+    def load_test_data(self, status_code, response_content: str):
+        self.test_data = {
+            u'期望状态码': int(status_code),
+            u'期望返回值': response_content,
+        }
+
+    def request_validation(self, method: str, path: str):
+        expected_status_code = self.test_data[u'期望状态码']
+        expected_response_content = self.test_data[u'期望返回值']
+        if method == 'GET':
+            response = requests.get(url=self.url + path)
+        elif method == 'POST':
+            response = requests.post(url=self.url + path)
+        else:
+            assert False, u'方法错误'
+        assert response.status_code == expected_status_code, \
+            u'状态码不符合预期: 预期=%s 实际=%s' % (expected_status_code, response.status_code)
+        print(u'状态码符合预期: %s' % response.status_code)
+        assert expected_response_content in str(response.content.decode('utf-8')), \
+            u'返回值不符合预期: 预期=%s 实际=%s' % (expected_response_content, response.content.decode('utf-8'))
+        print(u'返回值符合预期: %s' % response.content.decode('utf-8'))
+        response.close()
+
+
+if __name__ == '__main__':
+    api = TestAPI()
+    t_url = 'http://172.16.111.31:31880'
+    api.init_test_env(url=t_url)
+    api.load_test_data(status_code='404', response_content='Not Found')
+    api.request_validation(method='GET', path='/404')
